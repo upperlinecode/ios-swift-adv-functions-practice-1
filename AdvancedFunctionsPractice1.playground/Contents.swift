512a84faf5bc57b1162ed919801a804ae7e131f9
carsonlevine <carsonglevine@me.com>
2016-07-08 15:29:44 -0400

    first commit

---
+++ AdvancedFunctionsPractice1.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Define the described functions so that they produce the correct output
+
+
+//1. Bio
+//Create a function called bio that accepts three parameters: a string called name, a string called homeTown, and an integer called age.
+//print(bio("Dave", name: "Pittsburgh", age: 18))
+//   prints:
+//   Name: Dave
+//   Home Town: Pittsburgh
+//   Age: 18
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//2. Pythagorean Theorem
+//Write a function called hypotenuse that takes in two double values representing the legs of a right triangle. It should return a double value, the length of the triangle's hypotenuse
+//print(hypotenuse(3.0, leg2: 4.0)) // prints 5.0
+//print(hypotenuse(8.0, leg2: 6.0)) // prints 10.0
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//3. Volume of a Cylinder
+//Create a function called cylinderVolume that takes in two parameters, radius and height. Give both parameters a default value of 1.0. The function should return the volume of the cylinder, which is equal to pi * radius^2 * height
+//print() // prints 3.14
+//print(5.0, height: 3.2) // prints 251.33
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//4. Cash Register
+//Create a function that calculates the cost of a customer's purchase. The function should be called cashRegister and should accept a double called pricePerItem and an integer called count, representing the number of item's purchased (our cash register won't be able to handle customers with more than one type of item). The default value for count should be 1. The function will return the total cost of the purchase.
+
+
+
+
+
+
+
+
+//Bonus: Add to the cash register function so that it takes in any number of item prices and corresponding counts, not just one item price and count.
+
+
+
+
+
+
+
+
